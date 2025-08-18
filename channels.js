6b82ff9b3d59870bfd1245c415c21ef5e94ccff9
Ryan Morales <Ryan.Morales@west-mec.org>
2025-08-18 13:54:21 -0700

    animal images

---
+++ channels.js
@@ -0,0 +1,19 @@
+const TV_Channels = {
+  static:
+    "https://images.pexels.com/videos/6923433/pexels-photo-6923433.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  cat: "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  dog: "https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  squirrel:
+    "https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  giraffe:
+    "https://images.pexels.com/photos/802112/pexels-photo-802112.jpeg?auto=compress&cs=tinysrgb&w=600",
+  elephant:
+    "https://images.pexels.com/photos/1054655/pexels-photo-1054655.jpeg?auto=compress&cs=tinysrgb&w=601",
+  turtle:
+    "https://images.pexels.com/photos/68744/loggerhead-turtle-sea-ocean-water-68744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  bird: "https://images.pexels.com/photos/326900/pexels-photo-326900.jpeg?auto=compress&cs=tinysrgb&w=600",
+  dragon:
+    "https://images.pexels.com/photos/4922009/pexels-photo-4922009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+};
+
+export default TV_Channels;
